68b7a2555958c380729438e49373542bff31d8f9
wagerc97 <wager.clemens@gmail.com>
2022-12-09 12:17:07 +0100

    update-csb.sh creates all log folders

--- src/maintenance/update-csb.sh
+++ src/maintenance/update-csb.sh
@@ -59,6 +59,11 @@ case $INPUT in
 		mkdir -pv ~/csb/logs/control/ondemand
 		mkdir -pv ~/csb/logs/control/reboot
 		mkdir -pv ~/csb/logs/single
+	  mkdir -pv ~/csb/logs/single/check-web
+	  mkdir -pv ~/csb/logs/single/check-usb
+	  mkdir -pv ~/csb/logs/single/unmount
+	  mkdir -pv ~/csb/logs/single/mount
+	  mkdir -pv ~/csb/logs/single/backup
 		sleep 1
 
 		# give permission to scripts
